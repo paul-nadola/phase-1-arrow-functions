bf2e6d2819c8f2ef4f68be9a0090c744dbd85cce
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-14 18:46:52 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+/*num1 = 2000
+num2 = 100
+const divide = (num1, num2) => num1/num2;*/
+const divide = () => {
+    return 2000/100;
+};
+console.log(divide())
+
+const square = x => x*x;
+
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
